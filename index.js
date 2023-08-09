c9692f5ddf4d67a667f9f2a7963e14a7931ae1c6
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-08-09 13:10:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const port = 3000;
 const hostname = '127.0.0.1';
+const port = 3000;
 
 const server = http.createServer((req, res =>{
   res.statusCode = 200;
@@ -12,4 +12,3 @@ const server = http.createServer((req, res =>{
 server.listen(port, hostname, () => {
   console.log('Server running at http://%{hostname}:${port}/');
 });
-
