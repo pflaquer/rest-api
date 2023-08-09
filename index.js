4c3f27d04b12209f0395a2444b45dfff62555e5c
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-08-09 13:10:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const port = 3000;
 const hostname = '127.0.0.1';
+
 const server = http.createServer((req, res =>{
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
